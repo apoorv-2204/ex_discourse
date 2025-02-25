855d8addf4036f2c809a317e02f6653cf727cc47
apoorv_gupta <apoorvgupta9999j@gmail.com>
2025-02-25 19:43:57 +0530

    add pre commit git hooks

--- config/config.exs
+++ config/config.exs
@@ -40,6 +40,11 @@ config :git_hooks,
   auto_install: true,
   verbose: true,
   hooks: [
+    pre_commit: [
+      tasks: [
+        {:cmd, "mix format --check-formatted"}
+      ]
+    ],
     pre_push: [
       tasks: [
         {:cmd, "mix clean"},
